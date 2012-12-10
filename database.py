f69d55f1a8bc308fa6837779b14f0d2c658a4b5c
Mark Striemer <mstriemer@gmail.com>
2012-12-09 21:21:18 -0600

    add postgres support

--- database.py
+++ database.py
@@ -1,8 +1,12 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-engine = create_engine("sqlite:///sweatitout.db")
+dev_db = "sqlite:///sweatitout.db"
+engine = create_engine(os.environ.get('DATABASE_URL', dev_db))
+
 db_session = scoped_session(sessionmaker(autocommit=False,
                                          autoflush=False,
                                          bind=engine))
