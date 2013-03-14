b5d261473b90b579123c7c8eec1b03525dd4af6d
Mark Striemer <mstriemer@gmail.com>
2013-03-14 11:07:20 -0500

    Remove fit fix

--- courses.py
+++ courses.py
@@ -75,6 +75,6 @@ boot_camp = Course(
         "https://maps.google.ca/maps?q=Revive+Fitness+Sage+Creek&hl=en&ll=49.833886,-97.049017&spn=0.019432,0.045447&sll=49.83444,-97.1521&sspn=0.621812,1.454315&hq=Revive+Fitness+Sage+Creek&t=m&z=15&iwloc=A",
         )
 
-current_courses = [yoga, fit_fix, winter_boot, winter]
+current_courses = [yoga, winter_boot, winter]
 old_courses = [boot_camp]
 courses = current_courses + old_courses
